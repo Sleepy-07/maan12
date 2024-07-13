5987bd5d18515301b4bd8b7b04895d75bca1bd3d
Sleepy-07 <sahilvrmji@gmail.com>
2024-07-13 17:54:56 +0530

    acitivity_lifecycle

--- app/src/main/java/com/example/maan1/MainActivity.kt
+++ app/src/main/java/com/example/maan1/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.maan1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var btn: Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
+        Toast.makeText(this,"On create ",Toast.LENGTH_SHORT).show()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -16,5 +21,42 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        btn=findViewById(R.id.info)
+        btn?.setOnClickListener {
+            var intent= Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+
     }
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this,"On start",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this,"On resume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this,"On stop", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this,"On destroy", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this,"On Restart ", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this,"On Pause", Toast.LENGTH_SHORT).show()
+    }
+
 }
